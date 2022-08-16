37e2381d2b67fe717637cc2b225cb6ce615d118c
Jack Kiwema <60797039+jackkiwema@users.noreply.github.com>
2022-08-17 00:28:28 +0300

    Updated styles

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,10 @@
     var tablinks = document.getElementsByClassName("tab-links");
     var tabcontents = document.getElementsByClassName("tab-contents");
     function opentab(tabname){
-        for(tablink of tablinks){
+        for(var tablink in tablinks){
             tablink.clasList.remove("active-link");
         }
-        for(tabcontent of tabcontents){
+        for(var tabcontent in tabcontents){
             tabcontent.clasList.remove("active-tab");
         }
         event.currentTarget.clasList.add("active-link");
@@ -35,4 +35,32 @@
             form.reset()
         })
         .catch(error => console.error('Error!', error.message))
+    });
+
+    
+    const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+    const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+    const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+    const header = document.querySelector('.header.container');
+    
+    hamburger.addEventListener('click',()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+    
+    document.addEventListener('scroll',()=>{
+        var scroll_position = window.scrollY;
+        if(scroll_position > 250){
+            header.style.backgroundColor = "#29323c";
+        }
+        else{
+            header.style.backgroundColor = "transparent";
+        }
+    });
+    
+    menu_item.forEach(item=>{
+        item.addEventListener('click',()=>{
+            hamburger.classList.toggle('active');
+            mobile_menu.classList.toggle('active');
+        });
     });
