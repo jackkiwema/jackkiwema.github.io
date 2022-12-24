947c212634b938bea500aa289bbf69fc8dfe29f4
jackkiwema <jackkiwema@gmail.com>
2022-12-24 09:03:56 +0000

    Updated background image

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
-let tablinks = document.querySelector(".tab-links");
-let tabcontents = document.querySelector(".tab-contents");
+let tablinks = document.querySelectorAll('.tab-links');
+let tabcontents = document.querySelectorAll('.tab-contents');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
@@ -27,10 +27,16 @@ menu_item.forEach(item=>{
     });
 });
 
-tablinks.addEventListener('click', ()=>{
-    tablinks.classList.toggle('active-link');
-    tabcontents.classList.toggle('active-tab');
-});
+function opentab(tabname){
+    for(let i = 0; i < tablinks.length; i++){
+        tablinks[i].classList.remove('active-link');
+    }
+    for(let j = 0; j < tabcontents.length; j++){
+        tabcontents[j].classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add('active-link');
+    document.querySelector(tabname).classList.add('active-tab');
+}
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbw5HQDJwzYJ4S93i6OySXMX9Ihkky3DtGtJPduX2MNpk744L4Q4E50BM7EKNO6PsYbRoQ/exec'
 const form = document.forms['submit-to-google-sheet']
